033ed1275d79052d02b274c47932451eb07de605
unknown <dikshadeo@gmail.com>
2015-12-06 15:03:22 +0530

    Commit

---
+++ TrainingExercises/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TrainingExercises.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult exercise1()
+        {
+            return View();
+        }
+
+        public ActionResult exercise2()
+        {
+            ViewBag.Message = "Employee description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
